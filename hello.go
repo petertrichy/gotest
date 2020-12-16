8c374c85ba4495312288a7c6c383e3689365a72c
Peter <petertrichy@gmail.com>
2020-12-16 21:24:32 +0530

    used greetings in spanish and french

--- hello.go
+++ hello.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 
+const french = "French"
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+
+func Hello(name string, language string) string {
     if name == "" {
         name = "World"
     }
+    if language == spanish {
+        return spanishHelloPrefix + name
+    }
+    if language == french {
+        return frenchHelloPrefix + name
+    }
+
     return englishHelloPrefix + name
 }
 
 func main() {
-    fmt.Println(Hello("world!"))
+    fmt.Println(Hello("world!",""))
 }
 
